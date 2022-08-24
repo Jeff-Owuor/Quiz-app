f6ade896cef2da9a2183083ff31baec0c149043d
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2022-08-24 15:17:26 +0300

    Static files configuration
    Add directory to settings
    Add templates directory to settings
    add templates folder
    add static folder

--- quizTime/settings.py
+++ quizTime/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'quizes',
+    'questions',
+    'results'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'quizTime.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR/ 'static',
+    BASE_DIR/'quizes'/'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
