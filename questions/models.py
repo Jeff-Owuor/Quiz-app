d33e18bc12e4f7554697da96169a546fb40908ba
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2022-08-24 16:31:49 +0300

    add questions class
    define get answers method
    define reverse db relationship in answers method
    define model fields

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,26 @@
+from pyexpat import model
 from django.db import models
+from quizes.models import Quiz
 
 # Create your models here.
+
+class Questions(models.Model):
+    text = models.CharField(max_length=300)
+    quiz = models.ForeignKey(Quiz,on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.text)
+    
+    def get_answers(self):
+        return self.answers_set.all()
+    
+# class Answers(models.Model):
+#     text = models.CharField(max_length=300)
+#     correct = models.BooleanField(default=False)
+#     question = models.ForeignKey(Questions,on_delete=models.CASCADE)
+#     created = models.DateTimeField(auto_now_add=True)
+    
+#     def __str__(self):
+#         return f"question: {self.question.text}, answer: {self.text}, correct: {self.correct}"
+    
