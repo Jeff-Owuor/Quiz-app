a024dd64a34ca8cbe8c32e348ef00985aa494792
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2022-08-24 16:16:31 +0300

    Add quizes model
    define string representation
    define get questions method
    define rows of our db

--- quizes/models.py
+++ quizes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+DIFF_CHOICES = (
+    ('easy','easy'),
+    ('medium','medium'),
+    ('hard','hard'),
+)
+
+class Quiz(models.Model):
+    name = models.CharField(max_length=150)
+    topic = models.CharField(max_length=150)
+    number_of_questions = models.IntegerField()
+    time = models.IntegerField(help_text='duration of the test in minutes')
+    required_score_to_pass = models.IntegerField(help_text='required score in %')
+    difficulty = models.CharField(max_length=6, choices=DIFF_CHOICES)
+    
+    def __str__(self):
+        return f"{self.name}-{self.topic}"
+    
+    def get_questions(self):
+        pass
