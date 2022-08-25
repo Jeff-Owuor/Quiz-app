e4a32ff0652e42462c75b42bd382e8876a9d58ba
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2022-08-25 13:05:13 +0300

    Create and register quiz data view
    Create a function view and append all the questions to questions list
    Loop through the answers related to the question at hand
    Append the answers to the answers array
    Return a JsonResponse
    Import the view
    Create a path containing the imported view

--- quizes/views.py
+++ quizes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Quiz
 from django.views.generic import ListView
+from django.http import JsonResponse
 # Create your views here.
 
 class QuizListView(ListView):
@@ -10,3 +11,17 @@ class QuizListView(ListView):
 def quiz_view(request,pk):
     quiz = Quiz.objects.get(pk=pk)
     return render(request,'quizes/quiz.html',{'obj':quiz})
+
+def quiz_data_view(request,pk):
+    quiz = Quiz.objects.get(pk=pk)
+    questions = []
+    for question in quiz.get_questions():
+        answers = []
+        for answer in question.get_answers():
+            answers.append(answer.text)
+        questions.append({str(question):answers})
+        
+    return JsonResponse({
+        'data': questions,
+        'time': quiz.time
+    })
